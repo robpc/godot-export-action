f1202beb76d4b278efe5995520fb1092a0507620
Rob Cannon <rpcann@gmail.com>
2021-03-28 16:00:38 -0400

    Added debug info

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,12 @@ export_name=$2
 directory=$3
 filname=$4
 
+echo "mkdir -p ${project_path}/${directory}"
 mkdir -p ${project_path}/${directory}
 
 output_path=${directory%%/}${directory:+/}$filname
 
+ls -la ${project_path}
+
+echo "godot --path /workspace/${project_path} --export ${export_name} ${output_path}"
 godot --path /workspace/${project_path} --export ${export_name} ${output_path}
